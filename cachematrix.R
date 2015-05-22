89503b4a2ef8853c105f5eaccc331ca44e797e8c
Kaniska Mandal <kaniska.mandal@gmail.com>
2015-05-22 00:44:48 -0700

    added logic to create a cachebaleMatrix and fetch data from cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,32 @@
-## Getting the Cached values in order to input into cacheSolve() function
-## via creating the cacheable matrix:
+## Provide a makeCacheMatrix to create a cacheableMatrix
+## cacheableMatrix <-  makeCacheMatrix(square_matrix_input)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(input = matrix()) {
+  m <- NULL
+  set <- function(x) {
+    input <<- x
+    m <<- NULL
+  }
+  get <- function() input
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Provide cacheSolve to fetch the Inverse of cachebaleMatrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(input, ...) {
+  m <- input$getInverse()
+  if(!is.null(m)) {
+    message("fetch cached data")
+    return(m)
+  }
+  # Create the inverted matrix when the cached matrix is not available.
+  data <- input$get()
+  m <- solve(data, ...)
+  input$setInverse(m)
+  m
 }
